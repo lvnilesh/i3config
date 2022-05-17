400df53a5ef70793467f2bae67548b2225387e3f
MrBeeBenson <airriflepellet@gmail.com>
2022-05-17 22:50:36 +0100

    update(configure.sh): Update packages installed

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install i3 i3blocks rofi dmenu scrot imagemagick terminator picom lxappearance pavucontrol thunar -y
+sudo apt install i3 i3blocks rofi dmenu scrot imagemagick terminator picom lxappearance pavucontrol thunar feh neofetch -y
 mv ~/dotfiles/config/i3 ~/.config/
 mv ~/dotfiles/config/terminator ~/.config/
 mv ~/dotfiles/config/rofi ~/.config
