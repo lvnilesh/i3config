0cb3863f5185cd2a34a71d715c84d3ee8c4ba74a
LV Nilesh <nilesh@cloudgeni.us>
2024-05-26 21:59:13 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 sudo pacman -Syu
+
+sudo pacman -S --noconfirm gdm gnome gnome-extra base xorg-server i3-wm i3status feh playerctl pulseaudio ttf-jetbrains-mono nvidia nvidia-utils vi openssh bluez git -y
+
 sudo pacman -S --noconfirm i3 i3blocks rofi dmenu scrot imagemagick terminator picom lxappearance pavucontrol thunar feh neofetch -y
 
 sudo rm -rf ~/.config/i3 ~/.config/terminator ~/.config/rofi ~/Documents/bash ~/Pictures/wallpapers ~/.bash_aliases
+
+sudo nvidia-xconfig # creates /etc/X11/xorg.conf
+
+sudo systemctl enable sshd
+sudo systemctl start sshd
+
+sudo systemctl enable gdm
+sudo systemctl start gdm
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
 
 mv ~/i3config/config/i3 ~/.config/
 mv ~/i3config/config/terminator ~/.config/
