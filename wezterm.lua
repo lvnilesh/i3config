32a085141ab893108b11bf920004208802c76e17
LV Nilesh <nilesh@cloudgeni.us>
2024-05-28 10:29:26 -0700

    Create wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,36 @@
+
+-- https://www.youtube.com/watch?v=uOnL4fEnldA&t=317s
+-- https://github.com/josean-dev/dev-environment-files/blob/main/.wezterm.lua
+
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#706b4e",
+	selection_fg = "#f3d9c4",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 1
+config.macos_window_background_blur = 8
+
+-- and finally, return the configuration to wezterm
+return config
