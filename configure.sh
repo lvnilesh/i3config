a652f0216cc4c0a8ff0374616dad0933bf470d61
LV Nilesh <nilesh@cloudgeni.us>
2024-05-28 10:51:23 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -38,6 +38,8 @@ mv ~/i3config/config/rofi ~/.config
 mv ~/i3config/bash ~/Documents/
 mv ~/i3config/wallpapers ~/Pictures/
 mv ~/i3config/.bash_aliases ~
+
+rm ~/.wezterm.lua
 ln -s ~/i3config/wezterm.lua ~/.wezterm.lua
 
 git reset --hard
